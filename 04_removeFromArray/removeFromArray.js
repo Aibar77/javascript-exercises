f7f989c08556337f5d32d2617d4fd3f8f68c6058
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-04-03 11:48:41 +0600

    complete exercises from 01 to 07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr1,...args) {
+  
+  return arr1.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
