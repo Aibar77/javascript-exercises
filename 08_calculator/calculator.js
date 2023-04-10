8eb68f7c30d34d51294714303ced40c83138cdd5
Mambet Aibar <109662937+Aibar77@users.noreply.github.com>
2023-04-10 19:15:50 +0600

    finish exercises 08-12

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr ? arr.reduce((total,num)=> total + num, 0) : 0;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr ? arr.reduce((total, num) => total * num, 1) : 0;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let sum = 1;
+  for (let i = num; i > 1; i--) {
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  for (let person of arr) {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = 2023;
+    }
+  }
+ return arr.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth) ? -1 : 1)[0]
 };
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+]
+console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
